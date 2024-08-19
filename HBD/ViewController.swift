9c719081e3644567fad983702dddc32710d4fcd3
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-08-19 21:07:10 +0900

    Init: 초기설정

---
+++ HBD/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HBD
+//
+//  Created by 김수경 on 8/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
